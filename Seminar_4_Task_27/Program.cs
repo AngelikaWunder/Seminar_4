830481fb872f1c4b0b607b448abfb5df8f43399d
Angelika <wunderange@rambler.ru>
2023-02-08 19:03:50 +0300

    add seminar 4 homework task 27

---
+++ Seminar_4_Task_27/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int Sumofnumber (int num)
+{
+    int sum = 0;
+    int rest = 1;
+    if ( num == 0)
+    {
+        sum = 0;
+    }
+    else
+    {
+        while (num > 9)
+        {
+            rest = num % 10;
+            sum = sum + rest;
+            num /= 10;
+        }
+        sum = sum + num;
+    }
+    return sum;
+}
+Console.Clear();
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр в числе равна {Sumofnumber(N)}");
