e3307bd96c14e199873da58b2e83a941ad8aa60d
Angelika <wunderange@rambler.ru>
2023-02-08 18:19:44 +0300

    add seminar 4 homework task 25

---
+++ Seminat_4_Task_25/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+double Exponent (int numA, int numB)
+{
+   double res = Math.Pow(numA, numB);
+   return res = Math.Round(res, 2);
+}
+Console.Write("Введите число А: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+Console.WriteLine($" -> {Exponent (A, B)}");
