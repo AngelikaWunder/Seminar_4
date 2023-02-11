ac9f7189f9739e63a31d38a5d96cedd2c2f923ee
Angelika <wunderange@rambler.ru>
2023-02-11 21:12:44 +0300

    add seminar 5 homework 36

---
+++ Seminar_5_Task_36/Program.cs
@@ -0,0 +1,26 @@
+﻿// адайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+Console.Clear();
+int[] GetArray (int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i=0; i < size; i++)
+    {
+    result[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return result;
+}
+int SumOddElements (int[] array)
+{
+    int sum = 0;
+    for (int i=0; i < array.Length; i++)
+    {
+        if ( i % 2 > 0) sum = sum + array[i];
+    }
+    return sum;
+}
+int[] array1 = GetArray(5, -9, 99);
+Console.WriteLine(String.Join(", ", array1));
+Console.WriteLine($"Сумма элементов,стоящих на нечетных позициях равна {SumOddElements(array1)}");
+
