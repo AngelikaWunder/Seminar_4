0ac9152aeb9fa56a4f547829cd618f76a72ad622
Angelika <wunderange@rambler.ru>
2023-02-27 13:14:46 +0300

    add changes task 68

--- Seminar_9_Task_68/Program.cs
+++ Seminar_9_Task_68/Program.cs
@@ -4,7 +4,6 @@
 // m = 3, n = 2 -> A(m,n) = 29
 int GetAkkerman(int m, int n)
 {
-    // if (m == 0 && n == 0) return 1;
     if (m == 0) return (n + 1);
     else if (m != 0 && n == 0) return GetAkkerman(m - 1, 1);
     else return GetAkkerman(m-1, GetAkkerman (m, n-1));
@@ -13,8 +12,6 @@ Console.Write("Введите число m = ");
 int m = int.Parse(Console.ReadLine()!);
 Console.Write("Введите число n = ");
 int n = int.Parse(Console.ReadLine()!);
-Console.WriteLine($"GetAkkerman(m, n)");
-// int[,] arrayResult = GetAkkerman(m, n);
 Console.WriteLine($" A({m}, {n}) = {GetAkkerman(m, n)}");
 
 
